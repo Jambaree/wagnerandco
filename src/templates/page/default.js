b7ee73940d566c056c34c95f31ba080da04c0377
Hamid Goudarzi <51421954+HamidGoudarzi1988@users.noreply.github.com>
2022-02-10 10:23:29 -0800

    fixing info about and default

--- src/templates/page/default.js
+++ src/templates/page/default.js
@@ -1,99 +1,96 @@
 import React from 'react'
-// import { graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 
-const DefaultPage = props => {
-  return <>jjjjjjjjjjjjjjjjjj</>
+// Ours
+import permittedSlug from '../../utils/permitted-slug'
+import PageWrapper from '../../components/PageWrapper'
+import Wrapper from '../../components/Wrapper'
+import Header from '../../components/Header'
+import YoastHelmet from '../../components/YoastHelmet'
+// import GutenbergBlocks from '../components/GutenbergBlocks'
+
+const WPPage = props => {
+  const data = props.data
+  const pageNode = data.wpPage
+
+  // TODO Add placeholder schema page for Gutenberg
+  //      blocks on generic pages
+  // <GutenbergBlocks blocks={pageNode.blocks} />
+
+  return (
+    <PageWrapper className="WPPage">
+      <YoastHelmet node={pageNode} url={data.wp.acfOptions.options.url} />
+      <Wrapper maxWidth={3}>
+        <Header
+          showTitle
+          title={pageNode.title}
+          subtitle={pageNode.template.acfPages.wcoPageSubtitlee}
+        />
+        <div dangerouslySetInnerHTML={{ __html: pageNode.content }} />
+      </Wrapper>
+    </PageWrapper>
+  )
 }
 
-export default DefaultPage
-// // Ours
-// import permittedSlug from '../../utils/permitted-slug'
-// import PageWrapper from '../../components/PageWrapper'
-// import Wrapper from '../../components/Wrapper'
-// import Header from '../../components/Header'
-// import YoastHelmet from '../../components/YoastHelmet'
-// // import GutenbergBlocks from '../components/GutenbergBlocks'
+export default WPPage
 
-// const DefaultPageTemplate = props => {
-//   const data = props.data
-//   const pageNode = data.wordpressPage
+export const pageQuery = graphql`
+  query WordpressPage($id: String!) {
+    wp {
+      acfOptions {
+        options {
+          url
+        }
+      }
+    }
+    wpPage(id: { eq: $id }) {
+      id
+      slug
+      title
+      content
+      # TODO
+      # blocks {
+      #   innerHTML
+      #   blockName
+      # }
+      template {
+        ... on WpDefaultTemplate {
+          templateName
+          acfPages {
+            wcoPageSubtitle
+          }
+        }
+      }
+      #  acf {
+      #    wco_page_subtitle
+      #  }
+      # yoast_meta {
+      #   yoast_wpseo_title
+      #   yoast_wpseo_metadesc
 
-//   if (!permittedSlug(pageNode.slug)) {
-//     return null
-//   }
+      #   # Facebook
+      #   yoast_wpseo_facebook_title
+      #   yoast_wpseo_facebook_description
+      #   yoast_wpseo_facebook_type
+      #   # yoast_wpseo_facebook_image {
+      #   #   id
+      #   #   localFile {
+      #   #     childImageSharp {
+      #   #       id
+      #   #       fluid(maxWidth: 1200) {
+      #   #         aspectRatio
+      #   #         src
+      #   #       }
+      #   #     }
+      #   #   }
+      #   # }
 
-//   // TODO Add placeholder schema page for Gutenberg
-//   //      blocks on generic pages
-//   // <GutenbergBlocks blocks={pageNode.blocks} />
-
-//   return (
-//     <PageWrapper className="WPPage">
-//       <YoastHelmet node={pageNode} url={data.page.options.url} />
-//       <Wrapper maxWidth={3}>
-//         <Header
-//           showTitle
-//           title={pageNode.title}
-//           subtitle={pageNode.acf.wco_page_subtitle}
-//         />
-//         <div dangerouslySetInnerHTML={{ __html: pageNode.content }} />
-//       </Wrapper>
-//     </PageWrapper>
-//   )
-// }
-
-// export default DefaultPageTemplate
-
-// export const CollectionQuery = graphql`
-//   query DefaultPage($id: String!) {
-//     page: wpPage(id: { eq: $id }) {
-//       options {
-//         url
-//       }
-//     }
-//     wordpressPage(id: { eq: $id }) {
-//       id
-//       wordpress_id
-//       slug
-//       title
-//       template
-//       content
-
-//       # TODO
-//       # blocks {
-//       #   innerHTML
-//       #   blockName
-//       # }
-
-//       acf {
-//         wco_page_subtitle
-//       }
-//       # yoast_meta {
-//       #   yoast_wpseo_title
-//       #   yoast_wpseo_metadesc
-
-//       #   # Facebook
-//       #   yoast_wpseo_facebook_title
-//       #   yoast_wpseo_facebook_description
-//       #   yoast_wpseo_facebook_type
-//       #   # yoast_wpseo_facebook_image {
-//       #   #   id
-//       #   #   localFile {
-//       #   #     childImageSharp {
-//       #   #       id
-//       #   #       fluid(maxWidth: 1200) {
-//       #   #         aspectRatio
-//       #   #         src
-//       #   #       }
-//       #   #     }
-//       #   #   }
-//       #   # }
-
-//       #   # yoast_wpseo_facebook_image
-//       #   # Twitter
-//       #   yoast_wpseo_twitter_title
-//       #   yoast_wpseo_twitter_description
-//       #   # yoast_wpseo_twitter_image
-//       # }
-//     }
-//   }
-// `
+      #   # yoast_wpseo_facebook_image
+      #   # Twitter
+      #   yoast_wpseo_twitter_title
+      #   yoast_wpseo_twitter_description
+      #   # yoast_wpseo_twitter_image
+      # }
+    }
+  }
+`

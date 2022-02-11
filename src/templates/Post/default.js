4f114019202317d80892fd0c3be2a1d954deceac
Hamid Goudarzi <51421954+HamidGoudarzi1988@users.noreply.github.com>
2022-02-11 11:17:44 -0800

    fix post template

--- src/templates/Post/default.js
+++ src/templates/Post/default.js
@@ -1,56 +1,54 @@
 import React from 'react'
-// import { graphql } from 'gatsby'
-const Post = props => {
-  return <></>
+import { graphql } from 'gatsby'
+
+// Ours
+import permittedSlug from '../../utils/permitted-slug'
+import PageWrapper from '../../components/PageWrapper'
+import Wrapper from '../../components/Wrapper'
+import Header from '../../components/Header'
+import YoastHelmet from '../../components/YoastHelmet'
+import GutenbergBlocks from '../../components/GutenbergBlocks'
+
+const PostTemplate = props => {
+  const data = props.data
+  const postNode = data.wpPost
+
+  if (!permittedSlug(postNode.slug)) {
+    return null
+  }
+
+  return (
+    <PageWrapper is="article" className="WPPost">
+      <YoastHelmet url={data.wp.acfOptions.options.url} node={postNode} />
+      <Wrapper maxWidth={3}>
+        <Header showTitle title={postNode.title} />
+        <GutenbergBlocks blocks={postNode.blocks} />
+      </Wrapper>
+    </PageWrapper>
+  )
 }
 
-export default Post
-// // Ours
-// import permittedSlug from '../../utils/permitted-slug'
-// import PageWrapper from '../../components/PageWrapper'
-// import Wrapper from '../../components/Wrapper'
-// import Header from '../../components/Header'
-// import YoastHelmet from '../../components/YoastHelmet'
-// import GutenbergBlocks from '../../components/GutenbergBlocks'
+export default PostTemplate
 
-// const Post = props => {
-//   const data = props.data
-//   const postNode = data.wordpressPost
-
-//   if (!permittedSlug(postNode.slug)) {
-//     return null
-//   }
-
-//   return (
-//     <PageWrapper is="article" className="WPPost">
-//       <YoastHelmet url={data.page.options.url} node={postNode} />
-//       <Wrapper maxWidth={3}>
-//         <Header showTitle title={postNode.title} />
-//         {/* <GutenbergBlocks blocks={postNode.blocks} /> */}
-//       </Wrapper>
-//     </PageWrapper>
-//   )
-// }
-
-// export default Post
-
-// export const pageQuery = graphql`
-//   query Post($id: String!) {
-//     page: wpPage(id: { eq: $id }) {
-//       options {
-//         url
-//       }
-//     }
-//     wordpressPost(id: { eq: $id }) {
-//       date
-//       slug
-//       title
-//       id
-//       # content
-//       # blocks {
-//       #   innerHTML
-//       #   blockName
-//       # }
-//     }
-//   }
-// `
+export const pageQuery = graphql`
+  query PostById($id: String!) {
+    wp {
+      acfOptions {
+        options {
+          url
+        }
+      }
+    }
+    wpPost(id: { eq: $id }) {
+      date
+      slug
+      title
+      id
+      # content
+      blocks {
+        name
+        originalContent
+      }
+    }
+  }
+`

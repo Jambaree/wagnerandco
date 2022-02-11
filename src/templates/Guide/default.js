2d4f5ca524bab842bae68cf2193f295f7460d6fd
Hamid Goudarzi <51421954+HamidGoudarzi1988@users.noreply.github.com>
2022-02-10 22:30:29 -0800

    move to page instead of archive

--- src/templates/Guide/default.js
+++ src/templates/Guide/default.js
@@ -1,116 +1,112 @@
 import React from 'react'
-// import { graphql } from 'gatsby'
-const Guide = () => {
-  return <></>
+import { graphql } from 'gatsby'
+
+// Ours
+import permittedSlug from '../../utils/permitted-slug'
+import PageWrapper from '../../components/PageWrapper'
+import Wrapper from '../../components/Wrapper'
+import WhitespaceHeaderCorners from '../../components/WhitespaceHeaderCorners'
+import YoastHelmet from '../../components/YoastHelmet'
+import GutenbergBlocks from '../../components/GutenbergBlocks'
+import SidebarNav from '../../components/SidebarNav'
+import gutenbergBlocksToNav from '../../utils/gutenberg-blocks-to-nav'
+
+const WPGuide = props => {
+  const data = props.data
+  const pageNode = data.wpGuide
+  // let sidebarItems = gutenbergBlocksToNav(pageNode.blocks)
+
+  if (!permittedSlug(pageNode.slug)) {
+    return null
+  }
+
+  // NOTE Don’t index WP Guide pages
+  // TODO Make this configurable—we aren’t reading it from the WP custom post
+  // type data, because then that removes some of the metadata we need to
+  // be sent along via the API
+
+  return (
+    <PageWrapper className="WPGuide pb4" is="article">
+      <YoastHelmet node={pageNode} url={data.wp.acfOptions.options.url}>
+        <meta name="robots" content="noindex" />
+      </YoastHelmet>
+      <WhitespaceHeaderCorners
+        title={pageNode.title}
+        date={props.showDate ? pageNode.date : undefined}
+        location=""
+        reverse={true}
+      />
+      {/* <SidebarNav items={sidebarItems} /> */}
+      <Wrapper maxWidth={3}>
+        {/* <GutenbergBlocks blocks={pageNode.blocks} /> */}
+      </Wrapper>
+    </PageWrapper>
+  )
 }
 
-export default Guide
-// // Ours
-// import permittedSlug from '../../utils/permitted-slug'
-// import PageWrapper from '../../components/PageWrapper'
-// import Wrapper from '../../components/Wrapper'
-// import WhitespaceHeaderCorners from '../../components/WhitespaceHeaderCorners'
-// import YoastHelmet from '../../components/YoastHelmet'
-// import GutenbergBlocks from '../../components/GutenbergBlocks'
-// import SidebarNav from '../../components/SidebarNav'
-// import gutenbergBlocksToNav from '../../utils/gutenberg-blocks-to-nav'
+export default WPGuide
 
-// const Guide = props => {
-//   const data = props.data
-//   const pageNode = data.wordpressWpGuides
-//   let sidebarItems = gutenbergBlocksToNav(pageNode.blocks)
+WPGuide.defaultProps = {
+  showDate: false,
+}
 
-//   if (!permittedSlug(pageNode.slug)) {
-//     return null
-//   }
+export const pageQuery = graphql`
+  query WordpressGuide($id: String!) {
+    wp {
+      acfOptions {
+        options {
+          url
+        }
+      }
+    }
+    wpGuide(id: { eq: $id }) {
+      id
+      slug
+      title
+      date
+      # content
+      blocks {
+        name
+        attributesJSON
+        innerBlocks {
+          dynamicContent
+          saveContent
+        }
+      }
+      # acf {
+      #   wco_page_subtitle
+      # }
+      featuredImage {
+        node {
+          id
+          altText
+          localFile {
+            childImageSharp {
+              id
+              fluid(maxWidth: 960) {
+                src
+                srcSet
+                sizes
+              }
+            }
+          }
+        }
+      }
+      # yoast_meta {
+      #   yoast_wpseo_title
+      #   yoast_wpseo_metadesc
 
-//   // NOTE Don’t index WP Guide pages
-//   // TODO Make this configurable—we aren’t reading it from the WP custom post
-//   // type data, because then that removes some of the metadata we need to
-//   // be sent along via the API
+      #   # Facebook
+      #   yoast_wpseo_facebook_title
+      #   yoast_wpseo_facebook_description
+      #   yoast_wpseo_facebook_type
+      #   # yoast_wpseo_facebook_image
 
-//   return (
-//     <PageWrapper className="WPGuide pb4" is="article">
-//       <YoastHelmet node={pageNode} url={data.page.options.url}>
-//         <meta name="robots" content="noindex" />
-//       </YoastHelmet>
-//       <WhitespaceHeaderCorners
-//         title={pageNode.title}
-//         date={props.showDate ? pageNode.date : undefined}
-//         location=""
-//         reverse={true}
-//       />
-//       <SidebarNav items={sidebarItems} />
-//       <Wrapper maxWidth={3}>
-//         {/* <GutenbergBlocks blocks={pageNode.blocks} /> */}
-//       </Wrapper>
-//     </PageWrapper>
-//   )
-// }
-
-// export default Guide
-
-// WPGuide.defaultProps = {
-//   showDate: false,
-// }
-
-// export const pageQuery = graphql`
-//   query Guide($id: String!) {
-//     page: wpPage(id: { eq: $id }) {
-//       options {
-//         url
-//       }
-//     }
-//     wordpressWpGuides(id: { eq: $id }) {
-//       id
-//       wordpress_id
-//       slug
-//       title
-//       date
-//       template
-//       # content
-//       # blocks {
-//       #   innerHTML
-//       #   blockName
-
-//       #   # For Gravity Forms, probably other Gutenberg block type
-//       #   attrs {
-//       #     formId
-//       #     wordpress_id
-//       #   }
-//       # }
-//       # acf {
-//       #   wco_page_subtitle
-//       # }
-//       featured_media {
-//         id
-//         alt_text
-//         localFile {
-//           childImageSharp {
-//             id
-//             fluid(maxWidth: 960) {
-//               src
-//               srcSet
-//               sizes
-//             }
-//           }
-//         }
-//       }
-//       # yoast_meta {
-//       #   yoast_wpseo_title
-//       #   yoast_wpseo_metadesc
-
-//       #   # Facebook
-//       #   yoast_wpseo_facebook_title
-//       #   yoast_wpseo_facebook_description
-//       #   yoast_wpseo_facebook_type
-//       #   # yoast_wpseo_facebook_image
-
-//       #   # Twitter
-//       #   yoast_wpseo_twitter_title
-//       #   yoast_wpseo_twitter_description
-//       #   # yoast_wpseo_twitter_image
-//       # }
-//     }
-//   }
-// `
+      #   # Twitter
+      #   yoast_wpseo_twitter_title
+      #   yoast_wpseo_twitter_description
+      #   # yoast_wpseo_twitter_image
+      # }
+    }
+  }
+`

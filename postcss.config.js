fc0c980c1505484279f8f84011a8372dfa0c8da2
renesauve <rdasauve@gmail.com>
2024-11-28 14:18:35 -0800

    tailwind and otehr stuff

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
     'postcss-import': {},
     'postcss-preset-env': { stage: 0 },
     'postcss-browser-reporter': {},

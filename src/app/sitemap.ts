db891b064cb4696b37aaef36848f04751ea35471
renesauve <rdasauve@gmail.com>
2024-12-06 11:34:40 -0800

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next/types'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const items = await generateSitemap({
-    postTypes: ['pages', 'weddings', 'info'],
+    postTypes: ['pages', 'weddings', 'info', 'services'],
   })
 
   return items
